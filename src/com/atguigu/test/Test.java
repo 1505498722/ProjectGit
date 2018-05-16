83fd340d730e202dfa144ea2d473b50a9af99393
1505498722 <39328547+1505498722@users.noreply.github.com>
2018-05-16 21:21:13 +0800

    print 123

--- src/com/atguigu/test/Test.java
+++ src/com/atguigu/test/Test.java
@@ -3,7 +3,7 @@ package com.atguigu.test;
 public class Test {
 
 	public static void main(String[] args) {
-                 Systom.out.prinln("123");
+                 System.out.println("123");
 	}
 
 }
